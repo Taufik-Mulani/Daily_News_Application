24551f3e6f43accf80b6501581afcd3797e18b44
Hacker <taufikmulani88@gmail.com>
2024-02-21 11:01:57 +0530

    Initial Commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar sticky-top navbar-expand-lg navbar-light " style={{background : "#E3E9EA"}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">Khabri</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div className="d-flex justify-content-end">
+                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul className=" navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active " aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/business">Business</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/entertainment">Entertainment</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/general">General</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/health">Health</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/science">Science</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/sports">Sports</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/technology">Technology</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function NewsItem(props) {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div>
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://c.ndtvimg.com/2020-05/gii7gb14_mumbai-generic_625x300_18_May_20.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()} </small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
